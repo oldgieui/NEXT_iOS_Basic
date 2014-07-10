a71d9410a48387562c894adac619b18396338669
oldgieui <flecher1@gmail.com>
2014-07-10 15:06:49 +0900

    Initial Commit

---
+++ week1/iOS_Basic_Week1_FileAndString/iOS_Basic_Week1_FileAndString/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  iOS_Basic_Week1_FileAndString
+//
+//  Created by 이건희 on 2014. 7. 10..
+//  Copyright (c) 2014년 nhnnext. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
