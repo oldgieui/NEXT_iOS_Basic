29b38ebc286a0ce176367501f476797eae38f992
oldgieui <flecher1@gmail.com>
2014-07-10 16:05:05 +0900

    #1 특정 디렉터리 내부를 뒤져라

--- week1/iOS_Basic_Week1_FileAndString/iOS_Basic_Week1_FileAndString/main.m
+++ week1/iOS_Basic_Week1_FileAndString/iOS_Basic_Week1_FileAndString/main.m
@@ -8,14 +8,26 @@
 
 #import <Foundation/Foundation.h>
 
+void DisplayAllFilesAtPath(NSString* path){
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+    NSLog(@"current path is %@ ", [fileManager displayNameAtPath:path]);
+    NSLog(@"%hhd",[fileManager fileExistsAtPath:path]);
+    if ([fileManager fileExistsAtPath:path]) {
+        NSArray* fileList = [fileManager contentsOfDirectoryAtPath:path error:nil];
+        for (int i = 0; i<fileList.count; i++) {
+            NSLog(@"%@", fileList[i]);
+        }
+    }
+
+}
+
+
+
 int main(int argc, const char * argv[])
 {
-
     @autoreleasepool {
-        
-        // insert code here...
-        NSLog(@"Hello, World!");
-        
+        NSString* path = @"/Users/oldgieui/Dropbox/NHNNEXT/2014-2/";
+        DisplayAllFilesAtPath(path);
     }
     return 0;
 }
