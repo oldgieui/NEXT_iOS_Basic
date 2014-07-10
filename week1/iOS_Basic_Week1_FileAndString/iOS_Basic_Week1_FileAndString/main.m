62d13ee90d202c88bf0a77d8a235994745b26f6b
oldgieui <flecher1@gmail.com>
2014-07-10 16:26:38 +0900

    #2. File Matcher 구현하기￼

--- week1/iOS_Basic_Week1_FileAndString/iOS_Basic_Week1_FileAndString/main.m
+++ week1/iOS_Basic_Week1_FileAndString/iOS_Basic_Week1_FileAndString/main.m
@@ -11,23 +11,44 @@
 void DisplayAllFilesAtPath(NSString* path){
     NSFileManager *fileManager = [NSFileManager defaultManager];
     NSLog(@"current path is %@ ", [fileManager displayNameAtPath:path]);
-    NSLog(@"%hhd",[fileManager fileExistsAtPath:path]);
     if ([fileManager fileExistsAtPath:path]) {
         NSArray* fileList = [fileManager contentsOfDirectoryAtPath:path error:nil];
         for (int i = 0; i<fileList.count; i++) {
             NSLog(@"%@", fileList[i]);
         }
     }
-
 }
 
+NSArray* GetAllFilesAtPath(NSString* path){
+    NSFileManager* fileManager = [NSFileManager defaultManager];
+    return [fileManager contentsOfDirectoryAtPath:path error:nil];
+}
 
+BOOL IsFileExist(NSString* fileName, NSString* path){
+    NSArray* fileList = GetAllFilesAtPath(path);
+    for (int i = 0; i<fileList.count; i++) {
+        if ([fileName isEqualToString: fileList[i]]) {
+            return true;
+        }
+    }
+    return false;
+}
 
 int main(int argc, const char * argv[])
 {
     @autoreleasepool {
-        NSString* path = @"/Users/oldgieui/Dropbox/NHNNEXT/2014-2/";
-        DisplayAllFilesAtPath(path);
+//        NSString* path = @"/Users/oldgieui/Dropbox/NHNNEXT/2014-2/";
+        NSString* path = @"/Users/oldgieui/FileSearchTest";
+
+//        DisplayAllFilesAtPath(path);
+        if (IsFileExist(@"1.txt", path)) {
+            NSLog(@"File exists.");
+        }
+        else{
+            NSLog(@"File not exists.");
+        }
+
+        
     }
     return 0;
 }
